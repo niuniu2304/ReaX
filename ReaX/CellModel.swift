5ab30a9768c75739bcfd1d611728789e768bfd5b
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-08-19 21:42:29 +0200

    [Feature] Create cellModel

---
+++ ReaX/CellModel.swift
@@ -0,0 +1,36 @@
+//
+//  CellModel.swift
+//  ReaX
+//
+//  Created by Yao on 19/08/2025.
+//
+
+import Foundation
+import SwiftData
+import SwiftUICore
+
+struct CellModel: Identifiable {
+    var id: String = UUID().uuidString
+    
+    enum cellStates {
+        case normal
+        case red
+        case green
+        case black
+        
+        var backgroundColor: Color {
+            switch self {
+            case .normal:
+                return .gray
+            case .red:
+                return .red
+            case .green:
+                return .green
+            case .black:
+                return .black
+            }
+        }
+    }
+    
+}
+
