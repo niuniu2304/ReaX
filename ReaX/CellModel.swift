6668c2cbed2a6d56702cb425c14a53d3c5d4a622
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-08-23 12:26:22 +0200

    [Feature] Add cellModel ton development

---
+++ ReaX/CellModel.swift
@@ -0,0 +1,37 @@
+//
+//  CellModel.swift
+//  ReaX
+//
+//  Created by Yao on 19/08/2025.
+//
+
+import Foundation
+import SwiftData
+import SwiftUICore
+
+struct CellModel: Identifiable {
+    let id: String = UUID().uuidString
+    var backgroundColor: cellStates
+    
+    enum cellStates {
+        case normal
+        case red
+        case green
+        case black
+        
+        var backgroundColor: Color {
+            switch self {
+            case .normal:
+                return .gray
+            case .red:
+                return .red
+            case .green:
+                return .green
+            case .black:
+                return .black
+            }
+        }
+    }
+    
+}
+
