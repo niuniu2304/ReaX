4d2fdedbfbb18bbe851c522422693921e2a85c27
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-08-24 18:38:20 +0200

    [Bug] Fix Merge conflict

--- ReaX/CellViewModel.swift
+++ ReaX/CellViewModel.swift
@@ -16,17 +16,17 @@ class CellViewModel: ObservableObject {
     
     // If on Tap
     func onTap(cellIndex: Int) {
-        if grid[cellIndex].cellState.backgroundColor == .red {
+        if grid[cellIndex].backgroundColor == .red {
             // Deduct points from the score variable (+ make the score variable an environment variable accessible by all are use a @Binding to change it's value
             // Maybe use an animation to transition from the color changes
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
-                self.grid[cellIndex].cellState = .black
+                self.grid[cellIndex].backgroundColor = .black
             }
-            self.grid[cellIndex].cellState = .normal
-        } else if grid[cellIndex].cellState.backgroundColor == .green{
+            self.grid[cellIndex].backgroundColor = .normal
+        } else if grid[cellIndex].backgroundColor == .green{
             // Add points to score
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
-                self.grid[cellIndex].cellState = .normal
+                self.grid[cellIndex].backgroundColor = .normal
             }
         } else {
             // Do nothing or deduct points but less than if it's red
