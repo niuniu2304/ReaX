d6a4101743bdab3c68d96e4ac58d67322a8a2b83
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-08-29 16:04:55 +0200

    [Clean] Add TODO's and check previous todo's that have been done

--- ReaX/ContentView.swift
+++ ReaX/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 import SwiftData
 
 /*
- TODO: Finish the GridView
- TODO: Finish the contentView with the best score, a timer, a play button(to start the game), the grid, and maybe a pause button on the bottom
  TODO: Make the logic for the contentView to work(start/stop button, play button).
- TODO: make grid view using a lazyVGrid(or something like that)
- TODO: add a button to go to the scoresView to view all the scores
  TODO: Finish the scoresView by adding a List(View) of all the previous scores and the best score on the top along side with when it was realised
- TODO:
+ TODO: Finish the life feature 3 life for each time we got one wrong we substract one life
+ TODO: Finish the gameOverView
  */
 
 struct ContentView: View {
