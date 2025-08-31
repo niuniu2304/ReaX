fb5b64ebd4e3c46ae0f6104008d6f18131bb2227
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-08-31 15:16:53 +0200

    [Feature] Create ChangeRandomCell (State) in GridViewModel, needs to be passed down to the other functions such as start. + need to add a random Time Intervall between each changes

--- ReaX/GridMVVM/GridViewModel.swift
+++ ReaX/GridMVVM/GridViewModel.swift
@@ -53,7 +53,37 @@ class GridViewModel: ObservableObject {
         }
     }
     
+    
     func changeRandomCells() {
+        // Need to add a random time intervall between each changes.
+        
+        let randomStates: [CellModel.cellStates] = [.green, .red, .black]
+        let randomCell: Int = Int.random(in: 0..<9)
+        let randomCell1: Int = Int.random(in: 0..<9)
+        guard let randomState = randomStates.randomElement() else {
+            print("Error: Couldn't get random State")
+            return
+        }
+        guard let randomState1 = randomStates.randomElement() else{
+            print("Error: Couldn't get a random state")
+            return
+        }
+        let randomTime: Double = Double.random(in: 0.2..<1)
+        let randomBool: Bool = Bool.random()
+        
+        if randomBool {
+            grid[randomCell].cellState = randomState
+            grid[randomCell1].cellState = randomState1
+            DispatchQueue.main.asyncAfter(deadline: .now() + randomTime){
+                self.grid[randomCell].cellState = .normal
+                self.grid[randomCell1].cellState = .normal
+            }
+        } else {
+            grid[randomCell].cellState = randomState
+            DispatchQueue.main.asyncAfter(deadline: .now() + randomTime){
+                self.grid[randomCell].cellState = .normal
+            }
+        }
         // Change 1 or 2 random Cells 'color' in the grid for a random periode of timer between 0.2 to 1 seconds it lasts then
         // Like grid[arbitrary number variable ].backgroundCOlor == (arbitrary color between red and green and black(-2 lives and more rare if possible)
         // Then AsyncAfter... (arbitraryTime):
