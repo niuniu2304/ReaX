b21c86b7459d6724928fa66722e70a1570828544
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-09-01 12:25:11 +0200

    [Feature] Create GameOverView

---
+++ ReaX/Views/GameOverView.swift
@@ -0,0 +1,18 @@
+//
+//  GameOverView.swift
+//  ReaX
+//
+//  Created by Yao on 01/09/2025.
+//
+
+import SwiftUI
+
+struct GameOverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameOverView()
+}
