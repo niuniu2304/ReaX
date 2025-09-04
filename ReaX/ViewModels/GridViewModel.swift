ecf9472f5971ed7b2f2cb968d4d9bfc4b770d901
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-09-04 20:54:01 +0200

    [Clean] Delete unecessary function

--- ReaX/ViewModels/GridViewModel.swift
+++ ReaX/ViewModels/GridViewModel.swift
@@ -20,24 +20,6 @@ class GridViewModel: ObservableObject {
         CellModel(cellState: .green), CellModel(), CellModel(cellState: .green),
     ]
     
-    // If on Tap
-    func onTap(cellIndex: Int) {
-        if grid[cellIndex].cellState == .red {
-            // Deduct points from the score variable (+ make the score variable an environment variable accessible by all are use a @Binding to change it's value
-            // Maybe use an animation to transition from the color changes
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
-                self.grid[cellIndex].cellState = .black
-            }
-            self.grid[cellIndex].cellState = .normal
-        } else if grid[cellIndex].cellState == .green{
-            // Add points to score
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
-                self.grid[cellIndex].cellState = .normal
-            }
-        } else {
-            // Do nothing or deduct points but less than if it's red
-        }
-    }
     
     
     func changeRandomCells() {
