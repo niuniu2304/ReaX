30c6b845c1b88fd6646f3616c02d1037faaa7587
Luo Yiqin <LuoYiqin2304@gmail.com>
2025-09-11 22:20:52 +0200

    [Clean] Delete the finished tasks

--- ReaX/ReaXApp.swift
+++ ReaX/ReaXApp.swift
@@ -8,12 +8,6 @@
 import SwiftUI
 import SwiftData
 
-/*
- TODO: Make the logic for the contentView to work(start/stop button, play button).
- TODO: Finish the scoresView by adding a List(View) of all the previous scores and the best score on the top along side with when it was realised
- TODO: Finish the life feature 3 life for each time we got one wrong we substract one life
- TODO: Finish the gameOverView
- */
 
 @main
 struct ReaXApp: App {
